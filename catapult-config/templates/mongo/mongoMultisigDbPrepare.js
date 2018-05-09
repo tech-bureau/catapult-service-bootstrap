42e4588fb969e62d8df196c4e5a72c8c912b7d8c
Rich Pelavin <rich.reactor8.com>
2018-05-09 00:38:58 +0000

    Clean up of config file positions

--- catapult-config/templates/mongo/mongoMultisigDbPrepare.js
+++
@@ -1,6 +0,0 @@
-(function prepareMultisigCollections() {
-	db.createCollection('multisigs');
-	db.multisigs.createIndex({ 'multisig.account': 1 }, { unique: true });
-
-	db.multisigs.getIndexes();
-})();
--- catapult-config/templates/peer_node/install_block_properties.sh
+++
@@ -1,7 +0,0 @@
-cd /tmp
-mkdir settings
-mkdir -p seed/mijin-test/00000
-dd if=/dev/zero of=seed/mijin-test/00000/hashes.dat bs=1 count=64
-cd settings
-/catapult/bin/catapult.tools.nemgen --nemesisProperties /tmp/.block-properties-file/block-properties-file
-cp -r /tmp/seed/mijin-test/* /data*
