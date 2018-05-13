b92c9620dcbac51f42458b0574ae77863e8adaa2
Rich Pelavin <rich.reactor8.com>
2018-05-13 18:42:10 +0000

    seperate-nemgen

--- dockerfiles/catapult/userconfig/install_block_properties.sh
+++
@@ -1,8 +0,0 @@
-properties_file=$1
-cd /tmp
-mkdir settings
-mkdir -p seed/mijin-test/00000
-dd if=/dev/zero of=seed/mijin-test/00000/hashes.dat bs=1 count=64
-cd settings
-/catapult/bin/catapult.tools.nemgen --nemesisProperties ${1}
-cp -r /tmp/seed/mijin-test/* /data*
--- dockerfiles/catapult/userconfig/prepare.sh
+++ dockerfiles/catapult/userconfig/prepare.sh
@@ -1,18 +1,13 @@
+sleep 4
 if [ ! -d /data ]; then
         echo "/data directory does not exist"
         exit 1
 fi
 if [ ! -d /data/00000 ]; then
-  if [ -f  /nemesis/block-properties-file.properties ]; then
-      echo "data directory is empty, initializing from block-properties-file"
-      /bin/bash /userconfig/install_block_properties.sh /nemesis/block-properties-file.properties
-  else
-      echo "nemgen boostrap from image not supported"
-      exit 1
-  fi
+   echo "nemgen boostrap needs to be run"
+   exit 1
 fi
 ls -alh /data
-# sleep 10
 cd /data
 exec /catapult/bin/catapult.server /userconfig
 
