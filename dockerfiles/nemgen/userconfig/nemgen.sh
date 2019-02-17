6e55ed2549f6a21a1fd7eeb790bc42081c8c1b58
Rich Pelavin <rich@reactor8.com>
2019-02-17 03:09:18 +0000

    updated nemgen.sh for cow to handle issue with mosaic ids

--- dockerfiles/nemgen/userconfig/nemgen.sh
+++ dockerfiles/nemgen/userconfig/nemgen.sh
@@ -1,3 +1,9 @@
+config_form() {
+  split=$(echo $1 | sed  's/\(.\)/\1 /g')
+  concat=$(printf "%c%c%c%c'" $(echo $split))
+  echo "0x${concat::-1}"
+}
+
 if [ ! -d /data ]; then
   echo "/data directory does not exist"
   exit 1
@@ -9,7 +15,17 @@ if [ ! -d /data/00000 ]; then
   mkdir -p seed/mijin-test/00000
   dd if=/dev/zero of=seed/mijin-test/00000/hashes.dat bs=1 count=64
   cd settings
-  /catapult/bin/catapult.tools.nemgen --nemesisProperties /nemesis/block-properties-file.properties --resources /userconfig/
+  ######## need to run twice and patch the mosaic ids
+  # first time to get cat.harvest nad cat.currency
+  /catapult/bin/catapult.tools.nemgen --nemesisProperties /nemesis/block-properties-file.properties 2> /tmp/nemgen.log
+  harvesting_mosaic_id=$(grep "cat.harvest" /tmp/nemgen.log | grep nonce  | awk -F=  '{split($0, a, / /); print a[9]}' | sort -u)
+  currency_mosaic_id=$(grep "cat.currency" /tmp//nemgen.log | grep nonce  | awk -F=  '{split($0, a, / /); print a[9]}' | sort -u)
+
+  # second time after replacing values for currencyMosaicId and harvestingMosaicId
+  sed -i "s/^harvestingMosaicId = .*/harvestingMosaicId = $(config_form ${harvesting_mosaic_id})/" /userconfig/resources/config-network.properties
+  sed -i "s/^currencyMosaicId = .*/currencyMosaicId = $(config_form ${currency_mosaic_id})/" /userconfig/resources/config-network.properties
+  /catapult/bin/catapult.tools.nemgen --nemesisProperties /nemesis/block-properties-file.properties
+
   cp -r /tmp/seed/mijin-test/* /data*
 else
   echo "no need to run nemgen"
