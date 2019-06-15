accd18c3f168761dca496693fb6ddfd35ad1898c
NEM Foundation Devs <greg@nem.foundation>
2019-06-15 14:58:26 +0000

    set nem.xem mosaic in nemgen.sh

--- cmds/docker/dockerfiles/nemgen/userconfig/nemgen.sh
+++ cmds/docker/dockerfiles/nemgen/userconfig/nemgen.sh
@@ -16,10 +16,10 @@ if [ ! -d /data/00000 ]; then
   dd if=/dev/zero of=seed/mijin-test/00000/hashes.dat bs=1 count=64
   cd settings
   ######## need to run twice and patch the mosaic ids
-  # first time to get cat.harvest nad cat.currency
+  # first time to get nem.xem nad nem.xem
   /catapult/bin/catapult.tools.nemgen  --resources /userconfig/ --nemesisProperties /nemesis/block-properties-file.properties 2> /tmp/nemgen.log
-  harvesting_mosaic_id=$(grep "cat.harvest" /tmp/nemgen.log | grep nonce  | awk -F=  '{split($0, a, / /); print a[9]}' | sort -u)
-  currency_mosaic_id=$(grep "cat.currency" /tmp//nemgen.log | grep nonce  | awk -F=  '{split($0, a, / /); print a[9]}' | sort -u)
+  harvesting_mosaic_id=$(grep "nem.xem" /tmp/nemgen.log | grep nonce  | awk -F=  '{split($0, a, / /); print a[9]}' | sort -u)
+  currency_mosaic_id=$(grep "nem.xem" /tmp/nemgen.log | grep nonce  | awk -F=  '{split($0, a, / /); print a[9]}' | sort -u)
 
   # second time after replacing values for currencyMosaicId and harvestingMosaicId
   sed -i "s/^harvestingMosaicId = .*/harvestingMosaicId = $(config_form ${harvesting_mosaic_id})/" /userconfig/resources/config-network.properties
